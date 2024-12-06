3a78727e3bcd5004a0a1af06f705c302c2b1e92c
richmondafari <afaririchmond1000@gmail.com>
2024-12-06 23:11:43 +0000

    optimized the achievements page for responsiveness on mobile devices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Functionality to togggle the hamburger button
 function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('open'); // Toggle the "open" class
